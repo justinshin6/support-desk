f7dc8bc1cf391efa7f685d72023ef3a634c057af
justinshin6 <justinshin@brown.edu>
2022-06-23 22:53:27 -0400

    first day

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
-console.log("Server...")
+// common syntax for express
+
+
+const express = require('express')
+const dotenv = require('dotenv').config()
+
+const PORT = process.env.PORT || 3000
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Wecome to the Support Desk API'})
+})
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
