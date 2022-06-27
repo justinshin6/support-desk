f3ce6f293670ea94488c5b27f17aacf86bf03d43
justinshin6 <justinshin@brown.edu>
2022-06-26 23:26:25 -0400

    finished login authentication

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,16 @@
 
 
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 
 const PORT = process.env.PORT || 3000
+
+
+// Connect to database
+connectDB()
 const app = express()
 
 app.use(express.json())
