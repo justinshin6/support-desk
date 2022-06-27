e9628654c78ae8486bdb8f76e2819afe48fea1ef
justinshin6 <justinshin@brown.edu>
2022-06-26 22:19:46 -0400

    added error handling

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,16 @@
 // @access Public 
 
 const registerUser = (req, res) => {
+    const {name, email, password} = req.body
+
+    // Validation
+    if(!name || !email || !password) {
+        res.status(400)
+        throw new Error('Please include all fields')
+    }
     res.send('Regiester Route')
+
+
 }
 
 // @desc Login a new user 
--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,13 @@
 
 const express = require('express')
 const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
 
 const PORT = process.env.PORT || 3000
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
     res.status(200).json({message: 'Wecome to the Support Desk API'})
@@ -14,5 +17,7 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
+app.use(errorHandler)
+
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
