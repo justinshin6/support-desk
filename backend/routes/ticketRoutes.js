4bebad15d24645d1dd7d0d13070bdcd320113952
justinshin6 <justinshin@brown.edu>
2022-07-06 21:56:07 -0400

    finished note functionality

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -3,6 +3,10 @@ const router = express.Router()
 const {getTickets, getTicket, createTicket, deleteTicket, updateTicket} = require('../controllers/ticketController')
 const {protect} = require('../middleware/authMiddleware')
 
+// Re-route into note router 
+const noteRouter = require('./noteRoutes')
+router.use('/:ticketId/notes', noteRouter)
+
 // in order to get user tickets, you need to be authenticated (protected)
 router.get('/', protect, getTickets)
 router.post('/', protect, createTicket)
