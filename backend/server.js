3dfb26af13616a2d1a15c76a8c1080113c634ba5
justinshin6 <justinshin@brown.edu>
2022-08-05 18:38:27 -0400

    update server

--- backend/server.js
+++ backend/server.js
@@ -37,4 +37,6 @@ if(process.env.NODE_ENV === 'production') {
 app.use(errorHandler)
 
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
